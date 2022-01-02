80fe83952137e53f8b1cfb57f507146d52e87a07
Pijook <lordbaby330@gmail.com>
2022-01-02 13:54:27 +0100

    Initial commit

---
+++ src/main/java/pl/pijok/api/API.java
@@ -0,0 +1,16 @@
+package pl.pijok.api;
+
+import org.bukkit.plugin.java.JavaPlugin;
+
+public class API extends JavaPlugin {
+
+    @Override
+    public void onEnable() {
+
+    }
+
+    @Override
+    public void onDisable() {
+
+    }
+}
